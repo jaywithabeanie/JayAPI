99e3f9b07edd85b328e7ccb80c412fcab16d5027
JayPeg <80066463+JayyPeg@users.noreply.github.com>
2021-05-13 15:01:30 +0200

    Create Inventory_Manager.java

---
+++ Managers/Inventory/Inventory_Manager.java
@@ -0,0 +1,38 @@
+package me.jaypeg.jayapi.Managers.Inventory;
+
+import me.jaypeg.jayapi.Managers.Inventory.Inventories.LobbyDuels.DuelsCompass;
+import me.jaypeg.jayapi.Managers.Inventory.Inventories.LobbyMain.GameCompass;
+import me.jaypeg.jayapi.Managers.Inventory.Utils.createInventory;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.HashMap;
+
+
+/** @ClassType Manager Class */
+/** @ClassInfo Manages Inventory utils and saved Inventories */
+
+public class Inventory_Manager {
+
+
+    /** --------------------------------------------------------------------------------------------------------------------------------------------------- */
+    /** @UtilType       Inventory Util                                                                                                                      */
+    /** @UtilInfo       Creates an Inventory from given data                                                                                                */
+    /** @ParameterInfo  • rowCount: Amount of rows in the Inventory                                                                                         */
+    /**                 • title: Title of the Inventory                                                                                                     */
+    /**                 • inventorySlot_itemData: HashMap containing all the data regarding the ItemStack for the corresponding Inventory slot (as Strings) */
+    /**                         - material: Material of the ItemStack                                                                                       */
+    /**                         - amount: Amount of the Item within the ItemStack                                                                           */
+    /**                         - type: Data type of the ItemStack (used for colored wool, durability on tools, etc.)                                       */
+    /**                         - displayName: Custom name displayed for the ItemStack                                                                      */
+    /**                         - lore: Lore of the ItemStack (use \n to add more lines)                                                                    */
+    /** --------------------------------------------------------------------------------------------------------------------------------------------------- */
+    /**                                                                                                                                                     */
+    public static Inventory createInventory(int rowCount, String title, HashMap<Integer, ItemStack> inventorySlot_itemStack) {
+        return createInventory.createInventory(rowCount, title, inventorySlot_itemStack);
+    }
+    /**                                                                                                                                                     */
+    /** --------------------------------------------------------------------------------------------------------------------------------------------------- */
+
+
+}
