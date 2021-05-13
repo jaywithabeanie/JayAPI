3670ed00b85a65e04396f97f1eebe3c4b89a14aa
JayPeg <80066463+JayyPeg@users.noreply.github.com>
2021-05-13 17:54:05 +0200

    Create createConfig.java

---
+++ Managers/Config/Utils/createConfig.java
@@ -0,0 +1,48 @@
+package me.jaypeg.jayapi.Managers.Config.Utils;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+
+
+/** @ClassType Util Class */
+/** @ClassInfo Creates a config File */
+
+public class createConfig {
+
+
+    /** ---------------------------------------------------------------------------------------------------------------------------------------------- */
+    /** @UtilType       Void Util                                                                                                                      */
+    /** @UtilInfo       Creates a config File                                                                                                          */
+    /** @ParameterInfo  • directoryPath: Path the config File will be created in (use . to separate directories - eg. ".ExamplePlugin.ExampleFolder.") */
+    /**                 • fileName: Name the config File will be given                                                                                 */
+    /** ---------------------------------------------------------------------------------------------------------------------------------------------- */
+    /**                                                                                                                                                */
+    public static void createConfig(String directoryPath, String fileName) {
+
+        // Determine Plugin Name & Directory Path
+        String pluginName = directoryPath.split("\\.")[1];
+        directoryPath = directoryPath.replaceFirst("\\." + pluginName, "").replace(".", File.separator) + File.separator;
+
+        // Create Config File
+        {
+            File file = new File(Bukkit.getServer().getPluginManager().getPlugin(pluginName).getDataFolder() + directoryPath, fileName + ".yml");
+            if (!file.exists()) {
+                file.getParentFile().mkdirs();
+                try {
+                    file.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            YamlConfiguration.loadConfiguration(file);
+        }
+
+    }
+    /**                                                                                                                                                */
+    /** ---------------------------------------------------------------------------------------------------------------------------------------------- */
+
+
+}
