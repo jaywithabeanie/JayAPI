60c45bd429544608b4c863e07081ef083709660a
JayPeg <80066463+JayyPeg@users.noreply.github.com>
2021-05-17 20:15:19 +0200

    Update Inventory_Manager.java

--- Managers/Inventory/Inventory_Manager.java
+++ Managers/Inventory/Inventory_Manager.java
@@ -1,7 +1,5 @@
 package me.jaypeg.jayapi.Managers.Inventory;
 
-import me.jaypeg.jayapi.Managers.Inventory.Inventories.LobbyDuels.DuelsCompass;
-import me.jaypeg.jayapi.Managers.Inventory.Inventories.LobbyMain.GameCompass;
 import me.jaypeg.jayapi.Managers.Inventory.Utils.createInventory;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
@@ -10,24 +8,24 @@ import java.util.HashMap;
 
 
 /** @ClassType Manager Class */
-/** @ClassInfo Manages Inventory utils and saved Inventories */
+/** @ClassInfo Manages Inventories */
 
 public class Inventory_Manager {
 
 
-    /** ---------------------------------------------------------------------------------------------------------------------- */
-    /** @UtilType       Inventory Util                                                                                         */
-    /** @UtilInfo       Creates an Inventory from given data                                                                   */
-    /** @ParameterInfo  • rowCount: Amount of rows in the Inventory                                                            */
-    /**                 • title: Title of the Inventory                                                                        */
-    /**                 • inventorySlot_itemStack: HashMap containing all ItemStacks and their corresponding Inventory slot    */
-    /** ---------------------------------------------------------------------------------------------------------------------- */
-    /**                                                                                                                        */
-    public static Inventory createInventory(int rowCount, String title, HashMap<Integer, ItemStack> inventorySlot_itemStack) {
-        return createInventory.createInventory(rowCount, title, inventorySlot_itemStack);
+    /** ------------------------------------------------------------------------------------------------------------------- */
+    /** @UtilType       Inventory Util                                                                                      */
+    /** @UtilInfo       Creates an Inventory with given data                                                                */
+    /** @ParameterInfo  • rowCount: Amount of rows in the Inventory                                                         */
+    /**                 • title: Title of the Inventory                                                                     */
+    /**                 • inventoryContent: HashMap containing all Inventory slots with their corresponding ItemStacks      */
+    /** ------------------------------------------------------------------------------------------------------------------- */
+    /**                                                                                                                     */
+    public static Inventory createInventory(int rowCount, String title, HashMap<Integer, ItemStack> inventoryContent) {
+        return createInventory.createInventory(rowCount, title, inventoryContent);
     }
-    /**                                                                                                                        */
-    /** ---------------------------------------------------------------------------------------------------------------------- */
+    /**                                                                                                                     */
+    /** ------------------------------------------------------------------------------------------------------------------- */
 
 
 }
