be4ed0535d0650ce8e7f7d364635d08447a34e01
JayPeg <80066463+JayyPeg@users.noreply.github.com>
2021-05-17 20:13:33 +0200

    Update createInventory.java

--- Managers/Inventory/Utils/createInventory.java
+++ Managers/Inventory/Utils/createInventory.java
@@ -1,8 +1,6 @@
 package me.jaypeg.jayapi.Managers.Inventory.Utils;
 
-import me.jaypeg.jayapi.Managers.ItemStack.ItemStack_Manager;
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
@@ -15,28 +13,23 @@ import java.util.HashMap;
 public class createInventory {
 
 
-    /** --------------------------------------------------------------------------------------------------------------------------------------------------- */
-    /** @UtilType       Inventory Util                                                                                                                      */
-    /** @UtilInfo       Creates an Inventory from given data                                                                                                */
-    /** @ParameterInfo  • rowCount: Amount of rows in the Inventory                                                                                         */
-    /**                 • title: Title of the Inventory                                                                                                     */
-    /**                 • inventorySlot_itemData: HashMap containing all the data regarding the ItemStack for the corresponding Inventory slot (as Strings) */
-    /**                         - material: Material of the ItemStack                                                                                       */
-    /**                         - amount: Amount of the Item within the ItemStack                                                                           */
-    /**                         - type: Data type of the ItemStack (used for colored wool, durability on tools, etc.)                                       */
-    /**                         - displayName: Custom name displayed for the ItemStack                                                                      */
-    /**                         - lore: Lore of the ItemStack (use \n to add more lines)                                                                    */
-    /** --------------------------------------------------------------------------------------------------------------------------------------------------- */
-    /**                                                                                                                                                     */
-    public static Inventory createInventory(int rowCount, String title, HashMap<Integer, ItemStack> inventorySlot_itemStack) {
+    /** ------------------------------------------------------------------------------------------------------------------- */
+    /** @UtilType       Inventory Util                                                                                      */
+    /** @UtilInfo       Creates an Inventory with given data                                                                */
+    /** @ParameterInfo  • rowCount: Amount of rows in the Inventory                                                         */
+    /**                 • title: Title of the Inventory                                                                     */
+    /**                 • inventoryContent: HashMap containing all Inventory slots with their corresponding ItemStacks      */
+    /** ------------------------------------------------------------------------------------------------------------------- */
+    /**                                                                                                                     */
+    public static Inventory createInventory(int rowCount, String title, HashMap<Integer, ItemStack> inventoryContent) {
 
         // Create Base Inventory
         Inventory inventory = Bukkit.createInventory(null, rowCount * 9, title);
 
-        // Add Item Data to Inventory
-        for (Integer inventorySlot : inventorySlot_itemStack.keySet()) {
+        // Add ItemStacks to Inventory
+        for (Integer inventorySlot : inventoryContent.keySet()) {
 
-            ItemStack itemStack = inventorySlot_itemStack.get(inventorySlot);
+            ItemStack itemStack = inventoryContent.get(inventorySlot);
 
             inventory.setItem(inventorySlot, itemStack);
 
@@ -46,8 +39,8 @@ public class createInventory {
         return inventory;
 
     }
-    /**                                                                                                                                                     */
-    /** --------------------------------------------------------------------------------------------------------------------------------------------------- */
+    /**                                                                                                                     */
+    /** ------------------------------------------------------------------------------------------------------------------- */
 
-  
+
 }
