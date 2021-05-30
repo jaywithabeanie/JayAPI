bdde6eb63a1f4a152526432e16e163fae9216c90
Darkw1nged <56140296+Darkw1nged@users.noreply.github.com>
2021-05-30 20:43:40 +0100

    Message
    Jay please just upload a chunk of classes i beg.

--- Managers/Message/Message_Manager.java
+++ Managers/Message/Message_Manager.java
@@ -1,24 +1,22 @@
 package me.jaypeg.jayapi.Managers.Message;
 
-import me.jaypeg.jayapi.Managers.Message.Utils.createMessage;
-import me.jaypeg.jayapi.Managers.Message.Utils.sendMessage;
+import me.jaypeg.jayapi.Managers.Message.Utils.MessageAnnouncer;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
 
+/** @ClassType Manager Class */
 
+/** @ClassInfo Manages Messages */
 public class Message_Manager {
 
-
-    public static String createMessage(Player sender, String messageText) {
-        return createMessage.createMessage(sender, messageText);
+    public static void MessageManager(World world, String Message) {
+        MessageAnnouncer.MessageAnnouncer(world, Message);
     }
 
-
-    public static void sendMessage(ArrayList<World> worlds, String messageText) {
-        sendMessage.sendMessage(worlds, messageText);
+    public static void MessageManager(String worldName, String Message) {
+        MessageAnnouncer.MessageAnnouncer(worldName, Message);
     }
-
 
 }
